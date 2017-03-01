55c960f35ae6ac3b26824ad37d2d4393384c46c1
spences10 <spences10apps@gmail.com>
2017-03-01 06:48:33 +0000

    Understanding state Loading data onto state onClick

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,39 @@ import React from 'react'
 import Header from './Header'
 import Order from './Order'
 import Inventory from './Inventory'
+import sampleFishes from '../sample-fishes'
 
 class App extends React.Component {
+  constructor () {
+    super()
+
+    this.addFish = this.addFish.bind(this)
+    this.loadSamples = this.loadSamples.bind(this)
+
+    // get initialState
+    this.state = {
+      fishes: {},
+      state: {}
+    }
+  }
+
+  addFish (fish) {
+    // update state
+    const fishes = {...this.state.fishes}
+    //  add in our fish
+    const timestamp = Date.now()
+    // add fish to updated state
+    fishes[`fish-${timestamp}`] = fish
+    // set state
+    this.setState({ fishes }) // no need to pass fishes: fishes
+  }
+
+  loadSamples () {
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
+
   render () {
     return (
       <div className='catch-of-the-day'>
@@ -11,7 +42,10 @@ class App extends React.Component {
           <Header tagline='Fresh Seafood Market' />
         </div>
         <Order />
-        <Inventory />
+        {/* And Pass addFish from here down to Inventory, same for loadSamples */}
+        <Inventory
+          addFish={this.addFish}
+          loadSamples={this.loadSamples} />
       </div>
     )
   }
