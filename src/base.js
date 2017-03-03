350bb2340caa51fe9f8830d8a396d2551987c6d1
spences10 <spences10apps@gmail.com>
2017-03-03 19:11:17 +0000

    Persist state with :fire:base

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base'
+
+const base = Rebase.createClass({
+  apiKey: 'AIzaSyBdsAt9d09OWdnaCNWr1la9mM4gSgQeyF8',
+  authDomain: 'catch-of-the-day-scott-spence.firebaseapp.com',
+  databaseURL: 'https://catch-of-the-day-scott-spence.firebaseio.com'
+})
+
+export default base
