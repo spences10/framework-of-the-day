12cab7d806240efc9b60a432c1f9cd07832b4d57
spences10 <spences10apps@gmail.com>
2018-02-06 18:59:04 +0000

    try to use next/link

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
+// import Link from 'next/link'
+
 import { ThemeProvider } from 'styled-components'
 
 import StorePicker from '../components/StorePicker'
 import App from '../components/App'
 import { theme } from '../theme/globalStyle'
+
+// const Main = () => {
+//   return(
+//     Link
+//   )
+// }
 
 export default () => (
   <ThemeProvider theme={theme}>
