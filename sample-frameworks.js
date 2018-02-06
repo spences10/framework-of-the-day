36bbd42247d87d1c0d382d13f199bf429dd10f7d
spences10 <spences10apps@gmail.com>
2018-02-06 11:21:29 +0000

    add sample frameworks

---
+++ sample-frameworks.js
@@ -0,0 +1,72 @@
+module.exports = {
+  framework1: {
+    name: 'React',
+    image: '',
+    desc: '',
+    price: 80,
+    status: 'available'
+  },
+  framework2: {
+    name: 'Angular',
+    image: '',
+    desc: '',
+    price: 100,
+    status: 'available'
+  },
+  framework3: {
+    name: 'Ember',
+    image: '',
+    desc: '',
+    price: 70,
+    status: 'available'
+  },
+  framework4: {
+    name: 'Meteor',
+    image: '',
+    desc: '',
+    price: 150,
+    status: 'available'
+  },
+  framework5: {
+    name: 'Vue',
+    image: '',
+    desc: '',
+    price: 120,
+    status: 'available'
+  },
+  framework6: {
+    name: 'Preact',
+    image: '',
+    desc: '',
+    price: 75,
+    status: 'available'
+  },
+  framework7: {
+    name: 'Svelte',
+    image: '',
+    desc: '',
+    price: 110,
+    status: 'available'
+  },
+  framework8: {
+    name: 'Next',
+    image: '',
+    desc: '',
+    price: 130,
+    status: 'available'
+  },
+  framework9: {
+    name: 'Gatsby',
+    image: '',
+    desc: '',
+    price: 85,
+    status: 'available'
+  },
+  framework10: {
+    name: 'Inferno',
+    image: '',
+    desc: '',
+    price: 95,
+    status: 'available'
+  }
+}
