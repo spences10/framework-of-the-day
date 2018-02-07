b34c72905351acc37714bca61f9e1508ec2ffaad
spences10 <spences10apps@gmail.com>
2018-02-07 10:08:37 +0000

    add load samples

--- sample-frameworks.js
+++ sample-frameworks.js
@@ -1,71 +1,72 @@
 module.exports = {
   framework1: {
     name: 'React',
-    image: '',
-    desc: '',
+    image: '/static/react.png',
+    desc: 'A JavaScript library for building user interfaces',
     price: 80,
     status: 'available'
   },
   framework2: {
     name: 'Angular',
-    image: '',
-    desc: '',
+    image: '/static/angular.png',
+    desc: 'One framework. Mobile & desktop.',
     price: 100,
     status: 'available'
   },
   framework3: {
     name: 'Ember',
-    image: '',
-    desc: '',
+    image: '/static/ember.png',
+    desc: 'A framework for creating ambitious web applications.',
     price: 70,
     status: 'available'
   },
   framework4: {
     name: 'Meteor',
-    image: '',
-    desc: '',
+    image: '/static/meteor.png',
+    desc: 'THE FASTEST WAY TO BUILD JAVASCRIPT APPS',
     price: 150,
     status: 'available'
   },
   framework5: {
     name: 'Vue',
-    image: '',
-    desc: '',
+    image: '/static/vue.png',
+    desc: 'The Progressive JavaScript Framework',
     price: 120,
     status: 'available'
   },
   framework6: {
     name: 'Preact',
-    image: '',
-    desc: '',
+    image: '/static/preact.png',
+    desc: 'Fast 3kB alternative to React with the same modern API.',
     price: 75,
     status: 'available'
   },
   framework7: {
     name: 'Svelte',
-    image: '',
-    desc: '',
+    image: '/static/svelte.png',
+    desc: 'The magical disappearing UI framework',
     price: 110,
     status: 'available'
   },
   framework8: {
     name: 'Next',
-    image: '',
-    desc: '',
+    image: '/static/next.png',
+    desc:
+      'Next.js is a minimalistic framework for server-rendered React applications.',
     price: 130,
     status: 'available'
   },
   framework9: {
     name: 'Gatsby',
-    image: '',
-    desc: '',
+    image: '/static/gatsby.png',
+    desc: 'Blazing-fast static site generator for React',
     price: 85,
     status: 'available'
   },
   framework10: {
     name: 'Inferno',
-    image: '',
-    desc: '',
+    image: '/static/inferno.png',
+    desc: 'Blazing-fast static site generator for React',
     price: 95,
     status: 'available'
   }
