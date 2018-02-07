c00e7db53dcb4118a41b30eef2bef1daedc69885
spences10 <spences10apps@gmail.com>
2018-02-07 16:14:03 +0000

    margin and padding

--- components/Header.js
+++ components/Header.js
@@ -2,8 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 
 const HeaderTop = styled.div`
+  margin: 1rem;
+  padding: 1rem;
   background: ${props => props.theme.white};
   border: 1px solid ${props => props.theme.black};
+  border-radius: 4px;
 `
 
 const Title = styled.h1`
@@ -11,6 +14,7 @@ const Title = styled.h1`
   padding: 0.5rem 0.5rem -2rem 0.5rem;
   font-family: 'Bungee Inline';
   font-size: 3rem;
+  font-weight: 100;
   text-transform: uppercase;
   text-align: center;
 `
