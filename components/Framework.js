e75fac765f90172375117fe53002b4f61d44b49a
spences10 <spences10apps@gmail.com>
2018-02-07 10:16:42 +0000

    add SC from frameworks

--- components/Framework.js
+++ components/Framework.js
@@ -2,15 +2,24 @@ import React from 'react'
 import styled from 'styled-components'
 
 const FrameworkList = styled.li``
+const FrameworkImage = styled.img``
+const FrameworkName = styled.h3``
+const FrameworkPrice = styled.span``
+const FrameworkDesc = styled.p``
+const AddToOrder = styled.button``
 
 class Framework extends React.Component {
   render() {
+    const { details } = this.props
     return (
       <FrameworkList>
-        <img
-          src={this.props.details.image}
-          alt={this.props.details.name}
-        />
+        <FrameworkImage src={details.image} alt={details.name} />
+        <FrameworkName>
+          {details.name}
+          <FrameworkPrice> {details.price} </FrameworkPrice>
+        </FrameworkName>
+        <FrameworkDesc> {details.desc} </FrameworkDesc>
+        <AddToOrder>Add to Order</AddToOrder>
       </FrameworkList>
     )
   }
