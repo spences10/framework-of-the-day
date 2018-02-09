e89282b8e4bee2dd715db9d3e7fc2093bd704b95
spences10 <spences10apps@gmail.com>
2018-02-09 13:15:49 +0000

    add inventory items from state

--- components/Inventory.js
+++ components/Inventory.js
@@ -20,13 +20,107 @@ const AddFrameworks = styled.button.attrs({
   text-align: center;
 `
 
-const InventoryTitle = styled.h2``
+const InventoryTitle = styled.h2`
+  text-align: center;
+  text-transform: uppercase;
+`
+
+const FrameworkEdit = styled.div`
+  margin: 1rem 0rem;
+  padding: 1rem 0rem;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: auto;
+  grid-template-areas:
+    'n p s'
+    'd d d'
+    'i i i'
+    'a a a';
+  background: ${props => props.theme.white};
+`
+
+const FrameworkInput = styled.input.attrs({
+  type: props => props.type || 'text',
+  placeholder: props => props.placeholder
+})`
+  grid-area: ${props => props.area};
+  width: 100%;
+  text-align: center;
+  border: 1px solid ${props => props.theme.black};
+`
+
+const FrameworkSelect = styled.select`
+  grid-area: s;
+  width: 100%;
+  text-align: center;
+`
+
+const FrameworkSelectOpt = styled.option``
+
+const FrameworkDescription = styled.textarea.attrs({
+  placeholder: props => props.placeholder || 'Framework Desc'
+})`
+  grid-area: d;
+  width: 100%;
+  text-align: center;
+  resize: none;
+`
 
 class Inventory extends React.Component {
+  constructor() {
+    super()
+    this.renderInventory = this.renderInventory.bind(this)
+  }
+  renderInventory(key) {
+    const framework = this.props.frameworks[key]
+    return (
+      <FrameworkEdit key={key}>
+        <FrameworkInput
+          area={'n'}
+          type={'text'}
+          name="name"
+          value={framework.name}
+        />
+        <FrameworkInput
+          area={'p'}
+          type={'text'}
+          name="price"
+          value={framework.price}
+        />
+        <FrameworkSelect
+          type={'text'}
+          name="status"
+          value={framework.status}
+        >
+          <FrameworkSelectOpt value="available">
+            Fresh!
+          </FrameworkSelectOpt>
+          <FrameworkSelectOpt value="unavailable">
+            Depreciated!
+          </FrameworkSelectOpt>
+        </FrameworkSelect>
+        <FrameworkDescription
+          type={'text'}
+          name="desc"
+          value={framework.desc}
+        />
+        <FrameworkInput
+          area={'i'}
+          type={'text'}
+          name="image"
+          value={framework.image}
+        />
+        {/* <AddItem>Add Item</AddItem> */}
+      </FrameworkEdit>
+    )
+  }
+
   render() {
     return (
       <InventoryWrapper>
-        <InventoryTitle>inventory</InventoryTitle>
+        <InventoryTitle>Inventory</InventoryTitle>
+        {Object.keys(this.props.frameworks).map(this.renderInventory)}
         <AddFrameworkForm addFramework={this.props.addFramework} />
         <AddFrameworks onClick={this.props.loadSamples}>
           Load Sample Frameworks
