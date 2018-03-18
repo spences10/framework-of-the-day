ad6a6485ef7a0c3a712c0e70fabc5f81ac04e0aa
spences10 <spences10apps@gmail.com>
2018-03-18 20:40:30 +0000

    add Login

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const LoginNav = styled.nav``
+
+const LoginTitle = styled.h2``
+
+const Login = () => (
+  <LoginNav>
+    <LoginTitle>Inventory Login</LoginTitle>
+  </LoginNav>
+)
+
+export default Login
