c4fef359f431fbdd701bab03a44775f6d6c393c4
spences10 <spences10apps@gmail.com>
2018-03-19 06:04:04 +0000

    add buttons

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,13 +2,35 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
+import { StyledButton as SB } from '../theme/components'
+
 const LoginNav = styled.nav``
 
 const LoginTitle = styled.h2``
 
+const LoginP = styled.h2``
+
+const GitHubButton = SB.extend``
+
+const TwitterButton = SB.extend``
+
+const FacebookButton = SB.extend``
+
 const Login = () => (
   <LoginNav>
     <LoginTitle>Inventory Login</LoginTitle>
+    <LoginP>Sign in to manage store inventory.</LoginP>
+    <GitHubButton onClick={() => this.props.authenticate('Github')}>
+      Log in with GitHub
+    </GitHubButton>
+    <TwitterButton onClick={() => this.props.authenticate('Twitter')}>
+      Log in with Twitter
+    </TwitterButton>
+    <FacebookButton
+      onClick={() => this.props.authenticate('Facebook')}
+    >
+      Log in with Facebook
+    </FacebookButton>
   </LoginNav>
 )
 
