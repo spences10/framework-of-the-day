89669c6e4789df785e33e38b781e301d671d1164
spences10 <spences10apps@gmail.com>
2018-04-06 22:00:13 +0100

    try to even

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Router from './components/Router'
+import { FrameworkProvider } from './FrameworksContext'
+
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<Router />, document.getElementById('root'))
+ReactDOM.render(
+  <FrameworkProvider>
+    <Router />
+  </FrameworkProvider>,
+
+  document.getElementById('root')
+)
 registerServiceWorker()
