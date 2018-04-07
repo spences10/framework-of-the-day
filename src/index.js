ce99f02495fa07c749deccc56ca9e7a7c2223291
spences10 <spences10apps@gmail.com>
2018-04-07 15:24:33 +0100

    remove provider from index

--- src/index.js
+++ src/index.js
@@ -2,15 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Router from './components/Router'
-import { FrameworkProvider } from './FrameworksContext'
 
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(
-  <FrameworkProvider>
-    <Router />
-  </FrameworkProvider>,
+ReactDOM.render(<Router />, document.getElementById('root'))
 
-  document.getElementById('root')
-)
 registerServiceWorker()
