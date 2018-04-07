db4a67e527ab4d51460328b5b68f728c5f556861
spences10 <spences10apps@gmail.com>
2018-04-07 14:56:10 +0100

    move to components

--- src/FrameworksContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react'
-
-const FrameworkContext = createContext({})
-
-export const FrameworkProvider = FrameworkContext.Provider
-
-export const FrameworkConsumer = FrameworkContext.Consumer
